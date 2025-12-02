14d9c039cf61baf89ce43a2c7c252f5ed85d3b16
Ostanin Vasilii <vostan97@gmail.com>
2025-12-02 18:54:35 +0300

    Swap functions

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 import math
-def get_hypotenuse(a, b):
-    return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 def get_area(a, b):
     return a * b + 1
+def get_hypotenuse(a, b):
+    return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 if __name__ == "__main__":
     print("Введите a:")
     a = int(input())
